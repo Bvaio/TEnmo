674173bcd25dfa99b0a05794eed731a2bb5d3f5d
Christina Jordan <christinambjordan@gmail.com>
2022-04-27 12:38:26 -0400

    updating capstone get balance

---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountJdbcDao.java
@@ -0,0 +1,28 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+
+@Component
+public class AccountJdbcDao implements AccountDao{
+private JdbcTemplate jdbcTemplate;
+public AccountJdbcDao(JdbcTemplate jdbcTemplate){
+    this.jdbcTemplate = jdbcTemplate;
+}
+
+    @Override
+    public BigDecimal getBalance(int id) {
+    String sql = ("SELECT balance FROM account WHERE account_id = ?");
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+        if(results.next()){
+            return results.getBigDecimal("balance");
+        }
+        return null;
+    }
+
+}
---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,30 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private int account_id;
+    private BigDecimal balance;
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Account(int account_id, BigDecimal balance) {
+        this.account_id = account_id;
+        this.balance = balance;
+    }
+}
