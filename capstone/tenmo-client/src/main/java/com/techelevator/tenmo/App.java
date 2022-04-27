1c94b51527efb2dcc681f8777c3a2cb3a0f7d5a3
Thomas Lee <thmasleemail@gmail.com>
2022-04-27 17:18:36 -0400

    reworked viewCurrentBalance, thanks Chris :D

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/App.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/App.java
@@ -14,10 +14,9 @@ public class App {
 
     private final ConsoleService consoleService = new ConsoleService();
     private final AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+    private final AccountService accountService = new AccountService( API_BASE_URL );
 
     private AuthenticatedUser currentUser;
-
-    private final AccountService accountService = new AccountService( API_BASE_URL );
 
     public static void main(String[] args) {
         App app = new App();
--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -3,36 +3,27 @@ package com.techelevator.tenmo.services;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.util.BasicLogger;
-import org.apiguardian.api.API;
-import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 import java.math.BigDecimal;
 
-
 public class AccountService {
-    private final String API_BASE_URL; /* = "http://localhost:8080/account/"; */
-//    private String authenticationToken;
-    private RestTemplate restTemplate = new RestTemplate();
-
-//    public void setAuthenticationToken( String token ) {
-//        this.authenticationToken = token;
-//    }
+    private final String API_BASE_URL;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public AccountService( String url ) {
-        this.API_BASE_URL = url;
+        API_BASE_URL = url;
     }
 
-    public BigDecimal viewCurrentBalance( AuthenticatedUser user ) {
+    public BigDecimal viewCurrentBalance( AuthenticatedUser currentUser ) {
         BigDecimal balance = null;
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType( MediaType.APPLICATION_JSON );
-        headers.setBearerAuth( user.getToken() );
+        headers.setBearerAuth( currentUser.getToken() );
         HttpEntity< Account > entity = new HttpEntity<>( headers );
         try {
             balance = restTemplate.exchange( API_BASE_URL + "account/balance", HttpMethod.GET, entity, BigDecimal.class ).getBody();
@@ -40,13 +31,6 @@ public class AccountService {
             BasicLogger.log( ex.getMessage() );
         }
         return balance;
-//        Account account = null;
-//        try {
-//            account = restTemplate.getForObject(API_BASE_URL + "/account/balance", Account.class );
-//        } catch ( RestClientResponseException ex ) {
-//            ex.getMessage();
-//        }
-//        return account.getBalance();
     }
 
 }
