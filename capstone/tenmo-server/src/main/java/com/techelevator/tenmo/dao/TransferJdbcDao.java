c5b40ce401657145e3e76b7dcadbd51122a5e061
Thomas Lee <thmasleemail@gmail.com>
2022-04-28 12:31:40 -0400

    cannot get send to work...404 and 500 error

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferJdbcDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferJdbcDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.text.Bidi;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +16,9 @@ public class TransferJdbcDao implements TransferDao {
     private JdbcTemplate jdbcTemplate;
     private AccountJdbcDao accountJdbcDao;
 
-    public TransferJdbcDao(JdbcTemplate jdbcTemplate, AccountJdbcDao accountJdbcDao ) {
+    public TransferJdbcDao(JdbcTemplate jdbcTemplate ) {
         this.jdbcTemplate = jdbcTemplate;
-        this.accountJdbcDao = accountJdbcDao;
+        this.accountJdbcDao = new AccountJdbcDao( jdbcTemplate );
     }
 
     @Override
@@ -47,11 +48,13 @@ public class TransferJdbcDao implements TransferDao {
 
 //    @Override
     public Transfer addTransfer(int from_id, int to_id, BigDecimal amount ) {
-        Transfer transfer = new Transfer();
+        Transfer transfer = null;
+
         String sql = "INSERT INTO transfer ( transfer_type_id, transfer_status_id, account_from, account_to, amount ) VALUES ( 1, 1, ?, ?, ? ) RETURNING transfer_id;";
-        SqlRowSet results = jdbcTemplate.queryForRowSet( sql, from_id, to_id, amount );
+        SqlRowSet results = jdbcTemplate.queryForRowSet( sql, getAccountIdFromUserId( from_id ), getAccountIdFromUserId( to_id ), amount );
 
         if ( results.next() ) {
+            transfer = new Transfer();
             transfer.setTransfer_id( results.getInt( "transfer_id" ) );
             transfer.setAccount_from(results.getInt("account_from"));
             transfer.setAccount_to(results.getInt("account_to"));
@@ -64,24 +67,18 @@ public class TransferJdbcDao implements TransferDao {
     @Override
     public boolean sendBucks( int from_id, int to_id, BigDecimal amount ) {
         Transfer transfer = null;
-        boolean toAccountSuccess = false;
-        boolean fromAccountSuccess = false;
+        boolean updateBalance = false;
         boolean updateStatusSuccess = false;
 
         if ( checkBalanceAccount( from_id, amount ) && isSameAccountId( from_id, to_id ) ) {
             transfer = addTransfer( from_id, to_id, amount );
-
-            String transferToAccount = "UPDATE account SET balance = balance + ? WHERE user_id = ?;";
-            toAccountSuccess = jdbcTemplate.update( transferToAccount, amount, to_id ) == 1;
-
-            String transferFromAccount = "UPDATE account SET balance = balance - ? WHERE user_id = ?;";
-            fromAccountSuccess = jdbcTemplate.update( transferFromAccount, amount, from_id ) == 1;
-
-            String updateStatus = "UPDATE transfer SET transfer_status_id = 2 WHERE transfer_id = ?";
-            updateStatusSuccess = jdbcTemplate.update( updateStatus, transfer.getTransfer_id() ) == 1;
+            updateBalance = updatedBalance( from_id, to_id, amount );
+            if ( updateBalance ) {
+                String updateStatus = "UPDATE transfer SET transfer_status_id = 2 WHERE transfer_id = ?";
+                updateStatusSuccess = jdbcTemplate.update(updateStatus, transfer.getTransfer_id()) == 1;
+            }
         }
-
-        return toAccountSuccess && fromAccountSuccess && updateStatusSuccess;
+        return updateStatusSuccess;
     }
 
 
@@ -93,4 +90,26 @@ public class TransferJdbcDao implements TransferDao {
         return from_id != to_id;
     }
 
+    private int getAccountIdFromUserId( int user_id ) {
+        int account_id = 0;
+        String sql = "SELECT account_id FROM account WHERE user_id = ( SELECT user_id FROM tenmo_user WHERE user_id = ? );";
+        SqlRowSet results = jdbcTemplate.queryForRowSet( sql, user_id );
+        if ( results.next() ) {
+            account_id = results.getInt( "account_id" );
+        }
+        return account_id;
+    }
+
+    private boolean updatedBalance( int from_id, int to_id, BigDecimal amount ) {
+        boolean toAccountSuccess = false;
+        boolean fromAccountSuccess = false;
+
+        String transferToAccount = "UPDATE account SET balance = ( balance + ? ) WHERE user_id = ?;";
+        toAccountSuccess = jdbcTemplate.update(transferToAccount, amount, to_id) == 1;
+
+        String transferFromAccount = "UPDATE account SET balance = ( balance - ? ) WHERE user_id = ?;";
+        fromAccountSuccess = jdbcTemplate.update( transferFromAccount, amount, from_id ) == 1;
+
+        return toAccountSuccess && fromAccountSuccess;
+    }
 }
