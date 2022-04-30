86a5e3f9f546d5719270d4574b875eb85257133f
Christina Jordan <christinambjordan@gmail.com>
2022-04-30 19:14:18 -0400

    testing

--- capstone/tenmo-server/src/test/java/com/techelevator/tenmo/dao/TransferJdbcDaoTest.java
+++ capstone/tenmo-server/src/test/java/com/techelevator/tenmo/dao/TransferJdbcDaoTest.java
@@ -21,64 +21,71 @@ public class TransferJdbcDaoTest extends BaseDaoTests {
 //    private static final User USER_3 = new User(Long.valueOf(502), "almostDone", "almostDone", "Pedro");
 
 
-//    private static final Transfer TRANSFER_1 = new Transfer(1000, 500, 601, new BigDecimal(50), "send", "pending" );
+    //    private static final Transfer TRANSFER_1 = new Transfer(1000, 500, 601, new BigDecimal(50), "send", "pending" );
 //    private static final Transfer TRANSFER_2 = new Transfer(1001, 501, 602, new BigDecimal(100), "send", "approved");
 //    private static final Transfer TRANSFER_3 = new Transfer(1002, 502, 502, new BigDecimal(100), "send", "pending");
 //    private static final Transfer TRANSFER_5 = new Transfer();
-
-    private TransferJdbcDao sut;
-
-    @BeforeClass
-    public static void setUp() {
-        dataSource = new SingleConnectionDataSource(); // opens and makes a single connection, requires you to close it though
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/tenmo");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres1");
-        dataSource.setAutoCommit( false );
-    }
+//    private static SingleConnectionDataSource dataSource;
+    private TransferJdbcDao transferJdbcDao;
+    private JdbcUserDao jdbcUserDao;
+    private AccountJdbcDao accountJdbcDao;
+    public Transfer testAddedTransfer;
 
     @Before
-    public void setUpData(){
-        String sql = "INSERT INTO tenmo_user (user_id, username, password_hash)\n" +
-                "VALUES (500, 'weLovesTests', 'unknown');\n" +
-                "INSERT INTO tenmo_user (user_id, username, password_hash)\n" +
-                "VALUES (501, 'sleepyTester', 'unknown');\n" +
-                "INSERT INTO tenmo_user (user_id, username, password_hash)\n" +
-                "VALUES (502, 'almostDone', 'unknown') ;\n" +
-                "INSERT INTO tenmo_user (user_id, username, password_hash)\n" +
-                "VALUES (503, 'funTimes', 'unknown');\n" +
-                "\n" +
-                "INSERT INTO account (account_id, user_id, balance) VALUES (400,500, 500) ;\n" +
-                "INSERT INTO account (account_id, user_id, balance) VALUES (401, 501, 1000) ;\n" +
-                "INSERT INTO account (account_id, user_id, balance) VALUES (402, 502, 500) ;\n" +
-                "INSERT INTO account (account_id, user_id, balance) VALUES (403, 503, 50) ;\n" +
-                "\n" +
-                "INSERT INTO transfer (transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount)\n" +
-                "VALUES (300, 1, 2, 400, 401, 50);\n" +
-                "INSERT INTO transfer (transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount)\n" +
-                "VALUES (301, 1, 2, 401, 402, 100);\n" +
-                "INSERT INTO transfer (transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount)\n" +
-                "VALUES (302, 1, 2, 402, 403, 100);";
+    public void setup(){
+        JdbcTemplate  jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcUserDao = new JdbcUserDao(jdbcTemplate);
+        accountJdbcDao = new AccountJdbcDao(jdbcTemplate);
+        transferJdbcDao = new TransferJdbcDao(jdbcTemplate, accountJdbcDao, jdbcUserDao);
+        testAddedTransfer = new Transfer(1, 2, 400,403, BigDecimal.valueOf(50));
+    }
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate( dataSource );
-        jdbcTemplate.update( sql );
-        accountJdbcDao = new AccountJdbcDao( jdbcTemplate );
+
+    @Test
+    public void userList_should_return_all_except_userId_500() {
+        boolean isInList = false;
+
+        List<User> testList = transferJdbcDao.userList(500);
+        Assert.assertEquals(3, testList.size());
+        for (User user : testList) {
+            isInList = user.getId() == 500;
+        }
+        Assert.assertFalse(isInList);
 
     }
 
     @Test
-    public void userList_should_return_all_except_current_user() {
-        List<User> testList = sut.userList(500);
-        Assert.assertEquals(2, testList.size());
+    public void transferList_should_return_list_of_transferHistory_for_userId501() {
 
+        List<Transfer> testTransferList = transferJdbcDao.transferList(501);
+        Assert.assertEquals(2, testTransferList.size());
+        int countFrom = 0;
+        int countTo = 0;
+        for(Transfer transfer: testTransferList) {
+            if (transfer.getUserNameFrom().equals("sleepyTester")) {
+                countFrom++;
+            }
+            if (transfer.getUserNameTo().equals("sleepyTester")) {
+                countTo++;
+            }
+        }
+        Assert.assertEquals(1, countFrom);
+        Assert.assertEquals(1, countTo);
     }
 
     @Test
-    public void transferList() {
-    }
+    public void addTransfer_should_increase_user() {
 
-    @Test
-    public void addTransfer() {
+        boolean testAddTransfer = transferJdbcDao.addTransfer(500, testAddedTransfer);
+        Assert.assertTrue(testAddTransfer);
+
+       int newId = testAddedTransfer.getTransfer_id();
+
+       Assert.assertTrue(newId == 3001);
+
+
+
+//       Assert.assertEquals(testAddedTransfer, testAddTransfer);
     }
 
     @Test
@@ -86,13 +93,4 @@ public class TransferJdbcDaoTest extends BaseDaoTests {
     }
 
 
-    @After
-    public void rollback() throws SQLException {
-        dataSource.getConnection().rollback();
-    }
-
-    @AfterClass
-    public static void closeDataSource() {
-        dataSource.destroy();
-    }
 }
